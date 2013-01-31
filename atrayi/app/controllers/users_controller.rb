e8451e9d63db05e5c1481f79c3fe15ef17900c70
ATRAYI <atrayi.sinha@gmail.com>
2013-01-31 21:59:27 +0530

    committed

--- atrayi/app/controllers/users_controller.rb
+++ atrayi/app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+def index
+	@users=User.all
+end
+def show
+	@user=User.find(params[:id])
+end
+def create
+	@user=User.create(params[:user])
+	redirect_to users_path 
+end
+def new
+	@user=User.new
+	
+end
+def destroy
+	user=User.find(params[:user])
+	user.destroy()
+	redirect_to index
+end
 end
