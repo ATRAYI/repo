07cb0db5318602ea62a8297a538e2c9ceadb540c
ATRAYI <atrayi.sinha@gmail.com>
2013-02-04 12:44:56 +0530

    mailer and validation

--- atrayi/app/models/user.rb
+++ atrayi/app/models/user.rb
@@ -2,14 +2,14 @@ class User < ActiveRecord::Base
 	has_many :posts
 	attr_accessible :name, :email, :roll, :dept, :age, :contact_no
 	validates_length_of :name, :minimum=>2
-	validates :name, :presence => true 
-	validates :email, :presence => true 
+	validates :name, :presence => true
+	validates :email, :presence => true, :uniqueness=>true
 		
-#before_create :signup
+before_create :name_capitalize
 
-#public
-#def name_capitalize
-#:name.capitalize
-#end
-#end
+protected
+def name_capitalize
+:name.capitalize
 end
+end
+
